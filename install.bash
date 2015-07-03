7313a3a099014529469ea552b4324b9b2c9e72e8
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-07-03 17:49:07 +0900

    support vim and vimrc

--- install.bash
+++ install.bash
@@ -15,9 +15,14 @@ DOTFILES="$PWD"
 for file in $(find .                                                   \
     -path "./.??*" -a                                                  \
     ! \( -name ".git" -prune \) -a                                     \
-    \( -path "./.config/fish/functions" -prune -print -o -print \) ); do
+    \(                                                                 \
+      \( -path "./.config/fish/functions" -o                           \
+         -path "./.vim/bundle/neobundle.vim" \)                        \
+      -prune -print -o -print \) ); do
   file=${file#./}
-  if [[ -d "$file" && "$file" != ".config/fish/functions" ]]; then
+  if [[ -d "$file" &&                      \
+    "$file" != ".config/fish/functions" && \
+    "$file" != ".vim/bundle/neobundle.vim" ]]; then
     run mkdir -pv "$HOME/$file"
   else
     run ln -sfnv "$DOTFILES/$file" "$HOME/$file"

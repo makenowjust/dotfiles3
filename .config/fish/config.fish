0ba398ae627dff8515c79974fbc1fbd1cad4de85
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-07-12 00:03:26 +0900

    Add many aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,79 +7,86 @@ source ~/.config/fish/env.fish
 # あとは対話シェル以外では実行しなくてもいい
 if status --is-interactive
 
-# notify_time以上時間がかかったとき、デスクトップに通知を行なう
-# (単位はミリ秒)
-set notify_time (math "30 * 1000")
+  # 起動時に何も表示しない
+  set fish_greeting
 
-function _prompt_notify -e fish_prompt
-  if test -n "$CMD_DURATION" -a "$CMD_DURATION" -gt $notify_time
-    set -l title
-    if test $status -eq 0
-      set title "Command finished"
-    else
-      set title "Command failed (status is $status)"
-    end
+  # notify_time以上時間がかかったとき、デスクトップに通知を行なう
+  # (単位はミリ秒)
+  set notify_time (math "30 * 1000")
 
-    notify-send -a "terminal" $title $history[1]
-  end
-end
-
-# aliasがバグってるとしか思えないので適当に直す
-# https://github.com/fish-shell/fish-shell/issues/393
-function alias --argument alias command
-    set -l cmd (echo $command | sed 's/ /\n/g')[1]
-    set -l prefix
-    if type -q $cmd
-      switch (type -t $cmd)
-        case builtin
-          set prefix builtin
-        case file
-          set prefix command
+  function _prompt_notify -e fish_prompt
+    if test -n "$CMD_DURATION" -a "$CMD_DURATION" -gt $notify_time
+      set -l title
+      if test $status -eq 0
+        set title "Command finished"
+      else
+        set title "Command failed (status is $status)"
       end
 
-      eval "function $alias; $prefix $command \$argv; end"
-      complete -c $alias -xa "(
-          set -l cmd (commandline -pc | sed -e 's/^ *\S\+ *//' );
-          complete -C\"$command \$cmd\";
-      )"
+      notify-send -a "terminal" $title $history[1]
     end
-end
+  end
 
-# 元からあるコマンドにオプションを追加
+  # aliasがバグってるとしか思えないので適当に直す
+  # https://github.com/fish-shell/fish-shell/issues/393
+  function alias --argument alias command
+      set -l cmd (echo $command | sed 's/ /\n/g')[1]
+      set -l prefix
+      if type -q $cmd
+        switch (type -t $cmd)
+          case builtin
+            set prefix builtin
+          case file
+            set prefix command
+        end
 
-alias rm 'rm -i'
-alias mv 'mv -i'
-alias cp 'cp -i'
-alias emacs 'emacs -nw'
-alias mkdir 'mkdir -p'
-alias xclip 'xclip -selection clipboard'
+        eval "function $alias; $prefix $command \$argv; end"
+        complete -c $alias -xa "(
+            set -l cmd (commandline -pc | sed -e 's/^ *\S\+ *//' );
+            complete -C\"$command \$cmd\";
+        )"
+      end
+  end
 
-# エイリアス
-alias cls 'clear'
+  # 元からあるコマンドにオプションを追加
 
-alias mh 'mv -t .'
-alias ch 'cp -t .'
+  alias rm 'rm -i'
+  alias mv 'mv -i'
+  alias cp 'cp -i'
+  alias emacs 'emacs -nw'
+  alias mkdir 'mkdir -p'
+  alias xclip 'xclip -selection clipboard'
 
-alias .. 'prevd'
-alias ,, 'nextd'
+  # エイリアス
+  alias cls 'clear'
 
-# git as git + hub
-alias git 'hub'
+  alias mh 'mv -t .'
+  alias ch 'cp -t .'
 
-alias gst 'git status -s -b'
-alias gcm 'git commit'
-alias gcmm 'gcm -m'
-alias gcma 'gcm -a'
-alias gad 'git add'
-alias gadp 'gad -p'
-alias gps 'git push'
-alias gpsu 'gps -u'
-alias gpl 'git pull'
-alias gf 'git fetch'
-alias gm 'git merge'
-alias gdiff 'git diff HEAD'
-alias gcl 'git clone --recursive'
-alias ginit 'git init'
-alias glog 'git log'
-alias greset 'git reset'
+  alias .. 'prevd'
+  alias ,, 'nextd'
+
+  # git as git + hub
+  alias git 'hub'
+
+  alias gst 'git status -s -b'
+  alias gcm 'git commit'
+  alias gcmm 'gcm -m'
+  alias gcma 'gcm -a'
+  alias gad 'git add'
+  alias gadp 'gad -p'
+  alias gps 'git push'
+  alias gpsu 'gps -u'
+  alias gpl 'git pull'
+  alias gf 'git fetch'
+  alias gm 'git merge'
+  alias gdiff 'git diff HEAD'
+  alias gcl 'git clone --recursive'
+  alias ginit 'git init'
+  alias glog 'git log'
+  alias greset 'git reset'
+  alias grm 'git rm'
+  alias gco 'git checkout'
+  alias gcob 'gco -b'
+
 end
